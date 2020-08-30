cfc95c7f265817229f72255d849e799f846c8ba3
JuEun-Li <wndms61@gmail.com>
2020-08-30 23:31:36 +0900

    객체의 참조

--- chap06_re/person5.js
+++ chap06_re/person5.js
@@ -3,4 +3,4 @@ let person = [{ name: "홍길동", age: 16 }, { name: "임꺽정", age: 18 }];
 person[0].name = "길동";
 console.log(person);
 
-console.log(person[0] == person[1]); // 두 객체의 참조가 다르다.
+console.log(person[0] == person[1]); // 두 객체의 참조가 다르다
