246b50f793feee851399710aedf0e405eafe0f85
JuEun-Li <wndms61@gmail.com>
2020-08-31 21:54:57 +0900

    속성탐색

--- chap06_re/for1.js
+++ chap06_re/for1.js
@@ -1,6 +1,6 @@
 let person = { name: "홍길동", age: 16 };
 
 for (let key in person) {
-    let value = person[key];
+    let value = person[key]; // 속성을 엑세스 하는 방법
     console.log("%s: %s", key, value);
 }
